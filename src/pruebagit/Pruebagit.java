7a64050db9764524a0d8bd02a9ab0c2813f4aed7
aphd3 <aphd3@hotmail.com>
2021-08-30 17:54:50 -0500

    Prueba commit nueva

--- src/pruebagit/Pruebagit.java
+++ src/pruebagit/Pruebagit.java
@@ -6,7 +6,7 @@ package pruebagit;
  */
 public class Pruebagit {
     public static void main(String[] args) {
-        System.out.println("Hola equipo");
+        System.out.println("Hola equipo ");
     }
     
 }
