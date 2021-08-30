d991dd473d45b0594fd8655f7d02d90ff9174b9f
aphd3 <aphd3@hotmail.com>
2021-08-30 17:20:52 -0500

    Prueba commit1

---
+++ src/pruebagit/Pruebagit.java
@@ -0,0 +1,12 @@
+package pruebagit;
+
+/**
+ *
+ * @author aphd3
+ */
+public class Pruebagit {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
