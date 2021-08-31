651376d167e50234606eb14c5b65466973a334f2
Dana Garces <nana.ggr84@gmail.com>
2021-08-30 21:09:08 -0500

    Commit de Dana

--- src/pruebagit/Pruebagit.java
+++ src/pruebagit/Pruebagit.java
@@ -7,6 +7,8 @@ package pruebagit;
 public class Pruebagit {
     public static void main(String[] args) {
         System.out.println("Hola equipo ");
+        
+        System.out.println("Hoooola :)");
     }
     
 }
